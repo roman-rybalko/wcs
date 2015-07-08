cc74948c717483e30a7b35a719180275fa28eee9
Roman Rybalko <devel@romanr.info>
2015-07-08 16:48:21 +0300

    Database\Relational\User sql upd for mysql

--- web_construction_set/database/relational/user.sql
+++ web_construction_set/database/relational/user.sql
@@ -1,2 +1,2 @@
-create table users (login varchar, passhash varchar, id integer primary key autoincrement);
+create table users (login varchar(32), passhash varchar(255), id integer primary key auto_increment);
 create unique index users_idx on users (login);
