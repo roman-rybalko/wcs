605839891a694644f9f9551c5f0c89d19b41c748
Roman Rybalko <devel@romanr.info>
2015-09-11 15:33:01 +0300

    Database\TableWrapper fields constrain

--- web_construction_set/database/table_wrapper.php
+++ web_construction_set/database/table_wrapper.php
@@ -3,27 +3,28 @@
 namespace WebConstructionSet\Database;
 
 class TableWrapper {
-	private $db, $table;
+	private $db, $table, $fields;
 
-	public function __construct(\WebConstructionSet\Database\Relational $db, $tableName) {
+	public function __construct(\WebConstructionSet\Database\Relational $db, $tableName, $fields = []) {
 		$this->db = $db;
 		$this->table = $tableName;
+		$this->fields = $fields;
 	}
 
 	public function select($what = [], $where = []) {
-		return $this->db->select($this->table, $what, $where);
+		return $this->db->select($this->table, $what, array_merge($this->fields, $where));
 	}
 
 	public function update($what, $where) {
-		return $this->db->update($this->table, $what, $where);
+		return $this->db->update($this->table, $what, array_merge($this->fields, $where));
 	}
 
 	public function insert($what) {
-		return $this->db->insert($this->table, $what);
+		return $this->db->insert($this->table, array_merge($this->fields, $what));
 	}
 
 	public function delete($where) {
-		return $this->db->delete($this->table, $where);
+		return $this->db->delete($this->table, array_merge($this->fields, $where));
 	}
 
 	public function predicate($predName, $value) {
