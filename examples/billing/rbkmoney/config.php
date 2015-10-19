4d6d25312a40a3ae520533749f5afbd9c442ee96
Roman Rybalko <devel@romanr.info>
2015-10-19 15:24:33 +0300

    removed Billing\Rbkmoney (is not able to test - application is declined)

--- examples/billing/rbkmoney/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (file_exists(dirname(__FILE__) . '/config.local.php'))
-	require_once dirname(__FILE__) . '/config.local.php';
-else
-	require_once dirname(__FILE__) . '/config.sample.php';
