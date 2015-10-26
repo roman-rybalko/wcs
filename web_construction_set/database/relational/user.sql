fdc13dce19c7b1580ba6507b70f21535ed67bbc0
Roman Rybalko <devel@romanr.info>
2015-10-26 20:58:50 +0300

    Database\Relational\User - increased db login size

--- web_construction_set/database/relational/user.sql
+++ web_construction_set/database/relational/user.sql
@@ -1,2 +1,2 @@
-create table users (login varchar(32) not null, passhash varchar(255) not null, id integer primary key auto_increment not null);
-create unique index users_idx on users (login);
+create table users (login varchar(256) not null, passhash varchar(256) not null, id integer primary key auto_increment not null);
+create unique index users_idx on users (login(32));
