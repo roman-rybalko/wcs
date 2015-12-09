97fe5907a2b4c72a1d46187b108fc79feaf8997f
Roman Rybalko <devel@romanr.info>
2015-12-09 18:43:08 +0300

    Database\Relational\User - access time

--- web_construction_set/database/relational/user.php
+++ web_construction_set/database/relational/user.php
@@ -3,52 +3,57 @@
 namespace WebConstructionSet\Database\Relational;
 
 class User implements \WebConstructionSet\Database\User {
-	private $db, $table;
+	private $table;
 
-	public function __construct(\WebConstructionSet\Database\Relational $db, $table = 'users') {
-		$this->db = $db;
-		$this->table = $table;
+	public function __construct(\WebConstructionSet\Database\Relational $db, $tableName = 'users') {
+		$this->table = new \WebConstructionSet\Database\Relational\TableWrapper($db, $tableName);
 	}
 
 	public function create($login, $password) {
-		return $this->db->insert($this->table, ['login' => $login, 'passhash' => password_hash($password, PASSWORD_DEFAULT)]);
+		return $this->table->insert(['login' => $login, 'passhash' => password_hash($password, PASSWORD_DEFAULT), 'time' => time()]);
 	}
 
 	public function check($login, $password) {
 		$id = null;
-		$data = $this->db->select($this->table, ['passhash', 'id'], ['login' => $login]);
-		if ($data && password_verify($password, $data[0]['passhash']))
+		$data = $this->table->select(['passhash', 'id'], ['login' => $login]);
+		if ($data && password_verify($password, $data[0]['passhash'])) {
 			$id = $data[0]['id'];
+			$this->table->update(['time' => time()], ['id' => $id]);
+		}
 		return $id;
 	}
 
 	public function getId($login) {
-		$data = $this->db->select($this->table, ['id'], ['login' => $login]);
-		if ($data)
+		if ($data = $this->table->select(['id'], ['login' => $login]))
 			return $data[0]['id'];
 		return null;
 	}
 
 	public function delete($id) {
-		return $this->db->delete($this->table, ['id' => $id]);
+		return $this->table->delete(['id' => $id]);
 	}
 
 	public function rename($id, $newLogin) {
-		return $this->db->update($this->table, ['login' => $newLogin], ['id' => $id]);
+		return $this->table->update(['login' => $newLogin, 'time' => time()], ['id' => $id]);
 	}
 
 	public function password($id, $newPassword) {
-		return $this->db->update($this->table, ['passhash' => password_hash($newPassword, PASSWORD_DEFAULT)], ['id' => $id]);
+		return $this->table->update(['passhash' => password_hash($newPassword, PASSWORD_DEFAULT), 'time' => time()], ['id' => $id]);
 	}
 
+	/**
+	 * Список пользователей
+	 * @return [][id => integer, login => string, time => integer]
+	 */
 	public function get($ids = null) {
+		$fields = ['id', 'login', 'time'];
 		if ($ids) {
 			$data = [];
 			foreach ($ids as $id)
-				if ($data1 = $this->db->select($this->table, ['id', 'login'], ['id' => $id]))
+				if ($data1 = $this->table->select($fields, ['id' => $id]))
 					$data = array_merge($data, $data1);
 		} else {
-			$data = $this->db->select($this->table, ['id', 'login']);
+			$data = $this->table->select($fields);
 			if (!$data)
 				$data = [];
 		}
--- web_construction_set/database/relational/user.sql
+++ web_construction_set/database/relational/user.sql
@@ -1,2 +1,2 @@
-create table users (login varchar(256) not null, passhash varchar(256) not null, id integer primary key auto_increment not null);
+create table users (login varchar(256) not null, passhash varchar(256) not null, id integer primary key auto_increment not null, time integer not null);
 create unique index users_idx on users (login(32));
